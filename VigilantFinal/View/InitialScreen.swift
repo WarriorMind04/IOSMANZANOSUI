1c313af2598d8cbec9679b40cbb0a33e55b901ec
Warrior Mind 04 <josemiguel.guerrerojim@gmail.com>
2025-06-07 14:26:05 -0600

    Model, view, viewmodel

---
+++ VigilantFinal/View/InitialScreen.swift
@@ -0,0 +1,18 @@
+//
+//  InitialScreen.swift
+//  VigilantFinal
+//
+//  Created by Jose Miguel Guerrero Jiménez on 07/06/25.
+//
+
+import SwiftUI
+
+struct InitialScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitialScreen()
+}
---
+++ VigilantFinal/ViewModel/firstcommit.swift
@@ -0,0 +1,8 @@
+//
+//  firstcommit.swift
+//  VigilantFinal
+//
+//  Created by Jose Miguel Guerrero Jiménez on 07/06/25.
+//
+
+import Foundation
